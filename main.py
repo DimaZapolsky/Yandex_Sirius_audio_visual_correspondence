068b77e2800b2371d718cdf5c92f6ff727cb5050
nickkartashev <nickkartashev@gmail.com>
2019-11-06 15:04:04 +0300

    Change loss function and use F.grid_sample instead of np.repeat()

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.models as models
+import torch.nn.functional as F
 
 NUM_EPOCHS = 10
 N = 2 #count of mixed videos
@@ -27,7 +28,7 @@ opt_V = optim.SGD(V.parameters(), lr=lr1)
 opt_U = optim.SGD(U.parameters(), lr=lr2)
 opt_G = optim.SGD(G.parameters(), lr=lr3)
 
-criterion = nn.BCELoss()
+criterion = nn.L1Loss()
 
 print_loss_freq = 50 #print loss every print_loss_freq batches.
 
@@ -58,6 +59,10 @@ if (device.type == 'cuda') and (ngpu > 1):
     V = nn.DataParallel(V, list(range(ngpu)))
     G = nn.DataParallel(G, list(range(ngpu)))
 
+
+loss_train = []
+loss_test = []
+
 for epoch in range(start_epoch, NUM_EPOCHS):
     for batch_n, data in enumerate(dataloader, 0):
         audio_sum = torch.sum(data[1], 1).to(device)
@@ -71,8 +76,8 @@ for epoch in range(start_epoch, NUM_EPOCHS):
 
             U_res = U(audio_sum)
             V_res = V(video)
-            G_res = G(V_res, U_res)#(bs, x, y, t, freq)
-            model_answer = torch.mul(G_res, audio_sum[None, 1, 1, None, None])#(bs, x, y, t, freq) * (bs, t, freq)
+            G_res = G(V_res, U_res)  # (bs, x, y, t, freq)
+            model_answer = torch.mul(G_res, audio_sum[None, 1, 1, None, None])  # (bs, x, y, t, freq) * (bs, t, freq)
 
             loss = criterion(torch.sum(model_answer, [1, 2]), data[1][:, i, :] / audio_sum)
             losses.append(loss)
@@ -81,7 +86,7 @@ for epoch in range(start_epoch, NUM_EPOCHS):
             opt_V.step()
             opt_U.step()
             opt_G.step()
-
+        loss_train.append(np.array(losses).mean())
 
         if (batch_n % print_loss_freq == 0):
             test_loss = []
@@ -98,7 +103,8 @@ for epoch in range(start_epoch, NUM_EPOCHS):
 
                         loss = criterion(torch.sum(model_answer, [1, 2]), data[1][:, i, :] / audio_sum)
                         test_loss.append(loss)
-            print('epoch [%d/%d]\t batch [%d/%d]\t. Train loss: %d,\t test loss: %d' % (epoch, NUM_EPOCHS, batch_n, batch_count, losses.mean()), test_loss.mean())
+            loss_test.append(np.array(test_loss).mean())
+            print('epoch [%d/%d]\t batch [%d/%d]\t. Train loss: %d,\t test loss: %d' % (epoch, NUM_EPOCHS, batch_n, batch_count, np.array(losses).mean()), np.array(test_loss).mean())
 
         if (batch_n % save_freq == 0):
             torch.save(U, PATH_U)
@@ -112,20 +118,34 @@ for epoch in range(start_epoch, NUM_EPOCHS):
                 picture = data[0][-1, 0, :, :, :, -1]
                 video = data[0][-1:, 0, :, :, :, :]
                 audio = data[1][-1:, 0, :]
+
                 U_sample_res = U(audio)
                 V_sample_res = V(video)
                 G_sample_res = G(V_sample_res, U_sample_res)
+
                 model_sample_answer = torch.mul(G_sample_res, audio[None, 1, 1, None, None])
+
                 pca = sklearn.decomposition.PCA(n_components=3)
                 vectors_square = model_sample_answer[-1, :, :, -1, :]
                 vectors_flatten = vectors_square.reshape(-1, vectors_square.shape()[-1]).numpy()
                 rgb = pca.fit_transform(vectors_flatten)
+
                 rgb_picture = np.reshape(rgb, vecrors_square.shape(0)[:2] + (-1,))
                 located_sound_picture = np.transpose(rgb_picture, [2, 0, 1])
-                full_sound = np.repeat(located_sound_picture, 16, axis=1)
-                full_sound = np.repeat(full_sound, 16, axis=2)
-                output = torch.permute(located_sound_picture * 0.2 + picture.numpy() * 0.8)
+                full_sound = torch.from_numpy(located_sound_picture)
+                full_sound = full_sound[None, :, :, :]
+
+                x_out = torch.zeros((1,) + picture.shape[1:] + (2,))
+                nx = np.linspace(-1, 1, picture.shape[1])
+                ny = np.linspace(-1, 1, picture.shape[2])
+                nxv, nyv = np.meshgrid(nx, ny)
+                x_out[:, :, :, 0] = torch.from_numpy(nxv)
+                x_out[:, :, :, 1] = torch.from_numpy(nyv)
+
+                located_sound_picture = F.grid_sample(full_sound, x_out)
+                output = (located_sound_picture * 0.2 + picture * 0.8)
+
                 fig = plt.figure(figsize=(8,8))
                 plt.axis("off")
-                plt.imshow(np.transpose(output, (1, 2, 0)))
+                plt.imshow(np.transpose(output.numpy(), (1, 2, 0)))
 
