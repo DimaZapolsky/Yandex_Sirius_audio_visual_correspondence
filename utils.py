bbf6038a6808796efccd04c532e227886167082e
nickkartashev <nickkartashev@gmail.com>
2019-11-06 14:28:18 +0300

    Add transform function in utils

---
+++ utils.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+def transform(input, batch_format=False, logbasis=21):
+    x, y = input.shape[-2:]
+    if batch_format:
+        if (len(input.shape) == 3):
+            input = input[:, None, :, :]
+    elif len(input.shape) == 3:
+        input = input[None, :, :, :]
+    else:
+        input = input[None, None, :, :]
+
+    xl = np.linspace(-1, 1, y)
+    yl = np.linspace(-1, 1, y)
+
+    xl = (np.power(logbasis, (xl + 1) / 2) - (logbasis // 2 + 1)) / (logbasis // 2)
+
+    xv, yv = np.meshgrid(xl, yl)
+
+    x_out = torch.zeros(input.shape[:1] + (y, y, 2))
+    x_out[:, :, :, 0] = torch.from_numpy(xv)
+    x_out[:, :, :, 1] = torch.from_numpy(yv)
+
+    return F.grid_sample(input, x_out)
+
+
