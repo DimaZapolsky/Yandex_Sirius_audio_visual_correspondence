59e36633ce45d2ade289925e36ee555d89570f3f
nickkartashev <nickkartashev@gmail.com>
2019-11-06 15:38:52 +0300

    Refactoring variables names.

--- models.py
+++ models.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 import torch
 
 class Video(nn.Module):
-    def __init__(self, K, H, W, T):
+    def __init__(self, K, height, width, frames_count):
         super(Video, self).__init__()
 
-        self.K = K
-        self.H = H
-        self.W = W
-        self.T = T
+        self.K = K '''Number of video and audio features'''
+        self.height = height
+        self.width = width
+        self.frames_count = frames_count
 
         self.main = nn.Sequential(*list(models.resnet18(pretrained=True).children())[:-2])
 
@@ -27,7 +27,7 @@ class Video(nn.Module):
 
         self.main.add_module("conv_k", nn.Conv2d(512, K, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)))
 
-        self.tmp_conv = nn.Conv3d(T, 1, kernel_size=(1, 1, 1))
+        self.tmp_conv = nn.Conv3d(frames_count, 1, kernel_size=(1, 1, 1))
 
         self.activation = nn.Sigmoid()
 
@@ -36,10 +36,10 @@ class Video(nn.Module):
         self.tmp_conv.apply(init)
 
     def forward(self, input):
-        x = input.reshape((-1, 3, self.H, self.W))
+        x = input.reshape((-1, 3, self.height, self.width))
         x = self.main(x)
-
-        x = x.reshape((-1, self.T, self.K, self.H // 16, self.W // 16))
+        print(x.shape)
+        x = x.reshape((-1, self.frames_count, self.K, self.height // 16, self.width // 16))
 
         x = self.tmp_conv(x).squeeze()
         x = self.activation(x)
@@ -47,24 +47,21 @@ class Video(nn.Module):
 
 
 class Generator(nn.Module):
-    def __init__(self, K, H, W, aud_H, aud_W):
+    def __init__(self, K):
         super(Generator, self).__init__()
 
-        self.K = K
-        self.H = H
-        self.W = W
-        self.aud_H = aud_H
-        self.aud_W = aud_W
+        self.K = K '''Number of video and audio features'''
 
         self.main = nn.Linear(K, 1)
 
         self.activation = nn.Sigmoid()
 
-    def forward(self, inputV, inputA): #inputV.shape = [bs, K, h // 16, w // 16] inputA.shape = [bs, K, audH, audW]
+    def forward(self, inputV, inputA):  # inputV.shape = [bs, K, h // 16, w // 16], inputA.shape = [bs, K, audH, audW]
         x = inputV[:, :, :, :, None, None] * inputA[:, :, None, None, :, :]
         x = x.permute((0, 2, 3, 4, 5, 1))
         x = self.main(x).squeeze()
 
         x = self.activation(x)
-        return x
+        return x  # x.shape = [bs, h // 16, w // 16, audH, audW]
+
 
