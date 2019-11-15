9d3f3ec3d979752e97455ce4d2acfc7c46da1625
nickkartashev <nickkartashev@gmail.com>
2019-11-15 13:17:45 +0300

    Minor changes

--- audio/base.py
+++ audio/base.py
@@ -19,7 +19,7 @@ class Unet(torch.nn.Module):
             output_channels=generated_features * 8,
             inner_input_channels=generated_features * 8,
             innermost=True,
-            dropout_p=dropout_p,
+            dropout_p=dropout_p
         )
 
         for _ in range(depth - self.min_depth):
@@ -28,26 +28,26 @@ class Unet(torch.nn.Module):
                 inner_input_channels=generated_features * 8,
                 submodule=self.unet_block,
                 use_dropout=use_dropout,
-                dropout_p=dropout_p,
+                dropout_p=dropout_p
             )
 
         self.unet_block = UnetBlock(
             output_channels=generated_features * 4,
             inner_input_channels=generated_features * 8,
             submodule=self.unet_block,
-            dropout_p=dropout_p,
+            dropout_p=dropout_p
         )
         self.unet_block = UnetBlock(
             output_channels=generated_features * 2,
             inner_input_channels=generated_features * 4,
             submodule=self.unet_block,
-            dropout_p=dropout_p,
+            dropout_p=dropout_p
         )
         self.unet_block = UnetBlock(
             output_channels=generated_features,
             inner_input_channels=generated_features * 2,
             submodule=self.unet_block,
-            dropout_p=dropout_p,
+            dropout_p=dropout_p
         )
         self.unet_block = UnetBlock(
             output_channels=feature_channels,
@@ -55,8 +55,9 @@ class Unet(torch.nn.Module):
             input_channels=1,
             submodule=self.unet_block,
             outermost=True,
-            dropout_p=dropout_p,
+            dropout_p=dropout_p
         )
+
         def init_weights(m):
             if isinstance(m, torch.nn.Conv2d):
                 m.weight.data.normal_(0.0, 0.001)
@@ -73,117 +74,90 @@ class Unet(torch.nn.Module):
         output_data = self.unet_block(output_data)
         return self.activation(output_data)
 
+    class UnetBlock(torch.nn.Module):
+        def __init__(
+                self,
+                output_channels, inner_input_channels,
+                input_channels=None, inner_output_channels=None,
+                outermost=False, innermost=False,
+                use_dropout=False, noskip=False,
+                submodule=None,
+                dropout_p=0.5
+        ):
+            super(UnetBlock, self).__init__()
+            self.outermost = outermost
+            self.noskip = noskip
+            use_bias = False
 
-class UnetBlock(torch.nn.Module):
-    def __init__(
-            self,
-            output_channels, inner_input_channels,
-            input_channels=None, inner_output_channels=None,
-            outermost=False, innermost=False,
-            use_dropout=False, noskip=False,
-            submodule=None,
-            dropout_p=0.5,
-    ):
-        super(UnetBlock, self).__init__()
-        self.outermost = outermost
-        self.noskip = noskip
-        use_bias = False
+            if input_channels is None:
+                input_channels = output_channels
+            if innermost:
+                inner_output_channels = inner_input_channels
+            elif inner_output_channels is None:
+                inner_output_channels = 2 * inner_input_channels
 
-        if input_channels is None:
-            input_channels = output_channels
-        if innermost:
-            inner_output_channels = inner_input_channels
-        elif inner_output_channels is None:
-            inner_output_channels = 2 * inner_input_channels
+            down_relu = torch.nn.LeakyReLU(0.2, True)
+            down_norm = torch.nn.BatchNorm2d(inner_input_channels)
 
-        down_relu = torch.nn.LeakyReLU(0.2, True)
-        down_norm = torch.nn.BatchNorm2d(inner_input_channels)
+            up_relu = torch.nn.ReLU(True)
+            up_norm = torch.nn.BatchNorm2d(output_channels)
+            up_sample = torch.nn.Upsample(
+                scale_factor=2,
+                mode='bilinear',
+                align_corners=True
+            )
 
-        up_relu = torch.nn.ReLU(True)
-        up_norm = torch.nn.BatchNorm2d(output_channels)
-        up_sample = torch.nn.Upsample(
-            scale_factor=2,
-            mode='bilinear',
-            align_corners=True
-        )
-
-        down_conv = torch.nn.Conv2d(
-            input_channels,
-            inner_input_channels,
-            kernel_size=4,
-            stride=2, padding=1,
-            bias=use_bias
-        )
-        down_conv2 = torch.nn.Conv2d(
-            inner_input_channels,
-            inner_input_channels,
-            kernel_size=3,
-            padding=1,
-        )
-        if outermost:
-            up_conv = torch.nn.Conv2d(
-                inner_output_channels,
-                output_channels,
-                kernel_size=3,
-                padding=1,
+            down_conv = torch.nn.Conv2d(
+                input_channels,
+                inner_input_channels,
+                kernel_size=4,
+                stride=2, padding=1,
                 bias=use_bias
             )
-            up_conv2 = torch.nn.Conv2d(
-                output_channels,
-                output_channels,
-                kernel_size=3,
-                padding=1
-            )
-            down = [down_conv, down_relu, down_conv2]
-            up = [up_relu, up_sample, up_conv, up_relu, up_conv2]
 
-        elif innermost:
-            up_conv = torch.nn.Conv2d(
-                inner_output_channels,
-                output_channels,
-                kernel_size=3,
-                padding=1,
-                bias=use_bias
-            )
-            up_conv2 = torch.nn.Conv2d(
-                output_channels,
-                output_channels,
-                kernel_size=3,
-                padding=1,
-                bias=use_bias
-            )
-            down = [down_relu, down_conv, down_relu, down_conv2]
-            up = [up_relu, up_sample, up_conv, up_norm, up_relu, up_conv2]
+            if outermost:
+                up_conv = torch.nn.Conv2d(
+                    inner_output_channels,
+                    output_channels,
+                    kernel_size=3,
+                    padding=1
+                )
+                down = [down_conv]
+                up = [up_relu, up_sample, up_conv]
 
-        else:
-            up_conv = torch.nn.Conv2d(
-                inner_output_channels,
-                output_channels,
-                kernel_size=3,
-                padding=1,
-                bias=use_bias
-            )
-            up_conv2 = torch.nn.Conv2d(
-                output_channels,
-                output_channels,
-                kernel_size=3,
-                padding=1,
-                bias=use_bias
-            )
-            down = [down_relu, down_conv, down_norm, down_relu, down_conv2]
-            up = [up_relu, up_sample, up_conv, up_norm, up_relu, up_conv2]
+            elif innermost:
+                up_conv = torch.nn.Conv2d(
+                    inner_output_channels,
+                    output_channels,
+                    kernel_size=3,
+                    padding=1,
+                    bias=use_bias
+                )
+                down = [down_relu, down_conv]
+                up = [up_relu, up_sample, up_conv, up_norm]
 
-        model = down
-        if submodule:
-            model += [submodule]
-        model += up
-        if use_dropout:
-            model += [torch.nn.Dropout(dropout_p)]
+            else:
+                up_conv = torch.nn.Conv2d(
+                    inner_output_channels,
+                    output_channels,
+                    kernel_size=3,
+                    padding=1,
+                    bias=use_bias
+                )
+                down = [down_relu, down_conv, down_norm]
+                up = [up_relu, up_sample, up_conv, up_norm]
 
-        self.model = torch.nn.Sequential(*model)
+            model = down
+            if submodule:
+                model += [submodule]
+            model += up
+            if use_dropout:
+                model += [torch.nn.Dropout(dropout_p)]
 
-    def forward(self, input_data):
-        if self.outermost or self.noskip:
-            return self.model(input_data)
+            self.model = torch.nn.Sequential(*model)
 
-        return torch.cat([input_data, self.model(input_data)], 1)
+        def forward(self, input_data):
+            if self.outermost or self.noskip:
+                return self.model(input_data)
+
+            return torch.cat([input_data, self.model(input_data)], 1)
